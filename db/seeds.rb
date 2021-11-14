73dbcae79b76f4eb15c263f911a9ae5823ec0419
HotMonkeyWings <devsony52@gmail.com>
2021-11-14 19:57:15 +0530

    New seed configs

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,6 @@
         Title: Faker::Book.title,
         Author: Faker::Book.author,
         Publisher: Faker::Book.publisher,
-        Year: Faker::Time.forward(days: 23, period: :morning)
+        Year: Faker::Number.between(from: 1800, to: 2022)
     })
 end
